ee7e83c4538d5f87e51aed4044c47f2d74cc1766
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-08-22 20:55:16 +0300

    new project note-website-ts

---
+++ note-website-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
