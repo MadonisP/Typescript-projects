f1eb7c9dbc7d15a70b70f1a2310448f4c2b87cc9
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-08-25 01:25:22 +0300

    user controller and more

--- msi-daily-backend/controller/User-Controller.ts
+++ msi-daily-backend/controller/User-Controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import { CreateUserInput } from "../schema/User-Schema";
 import { createUser } from '../service/User-Service'
 import logger from '../utils/logger'
+import { omit } from 'lodash'
 
-export async function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(req: Request<{}, {}, CreateUserInput['body']>, res: Response) {
     try {
         const user = await createUser(req.body)
-        return user;
+        return res.send(omit(user.toJSON(), "password"));
     } catch (e: any) {
         logger.error(e)
         return res.status(409).send(e.massage)
