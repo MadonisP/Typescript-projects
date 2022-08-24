b0b4439ab40ed7c1c9d64cebd8d30014f0623ba9
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-08-25 00:24:44 +0300

    backend update

--- msi-daily-backend/config/default.ts
+++ msi-daily-backend/config/default.ts
@@ -1,4 +1,5 @@
-export default{
-    port:5000,
-    dbUri: "mongodb+srv://MadonisP:iki002Mm@quizapp.eusnci7.mongodb.net/msiDaily?retryWrites=true&w=majority"
+export default {
+    port: 5000,
+    dbUri: "mongodb+srv://MadonisP:iki002Mm@quizapp.eusnci7.mongodb.net/msiDaily?retryWrites=true&w=majority",
+    saltWorkFactor: 10,
 }
